afc863a26cf4891b3b20b00356437172d6885fad
Xecus <imkkai@hotmail.com>
2020-05-08 21:37:18 +0800

    修改id为只读

--- src/interface/IBotDriver.ts
+++ src/interface/IBotDriver.ts
@@ -4,7 +4,7 @@ import { IBotEvent } from './IBotEvent';
 
 export interface IBotDriver{
     //driver id，用于寻找driver
-    id: string;
+    readonly id: string;
     //检查是否适用于该连接， 同时处理连接参数
     canUse( req: IncomingMessage ): Boolean;
     //调用API
